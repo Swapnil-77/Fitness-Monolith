e616a7fdbcba3d8f99dd4aa7a474748ab9d290bf
Swapnil phad <swapnilphad07@gmail.com>
2026-05-16 23:28:11 +0530

    Update

--- src/main/java/com/controller/ActivityController.java
+++ src/main/java/com/controller/ActivityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "**")
 @RequestMapping("/api/activities")
 @RequiredArgsConstructor
 public class ActivityController {
